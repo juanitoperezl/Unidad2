0901557eb324c15c4e5799dbf2763e27d90054ea
juanitoperezl <143725158+juanitoperezl@users.noreply.github.com>
2023-09-07 08:54:11 -0600

    Add files via upload
    Area de Rectangulo

---
+++ Practica2.0.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        int base = 10;
+        int altura = 5;
+        int areaRectangulo = base*altura;
+        System.out.println("MI nombre es Jaziel Peña Pino");
+        System.out.println("la base del rectangulo es: " + areaRectangulo);
+        
+    }
+    
+}
